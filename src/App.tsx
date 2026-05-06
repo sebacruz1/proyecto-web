0c0a7c0906eb5bfacd10d25011f106e061e65900
sebacruz1 <sebastiancruzpomar@gmail.com>
2026-05-06 19:06:39 -0400

    Uso de tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import Login from "./pages/public/Login";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/public/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
